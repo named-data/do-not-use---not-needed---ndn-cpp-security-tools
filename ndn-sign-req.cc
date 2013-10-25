185246488f90ec6a15bce4624c89bbed0e42a93b
Yingdi Yu <yuyingdi@gmail.com>
2013-10-25 11:42:55 -0700

    change options of ndn-sign-req

--- ndn-sign-req.cc
+++ ndn-sign-req.cc
@@ -27,16 +27,21 @@ namespace po = boost::program_options;
 
 int main(int argc, char** argv)	
 {
-  string keyName;
+  string name;
+  bool isKeyName = false;
 
-  po::options_description desc("General options");
+  po::options_description desc("General Usage\n  ndn-sign-req [-h] [-k] name\nGeneral options");
   desc.add_options()
     ("help,h", "produce help message")
-    ("key_name,n", po::value<string>(&keyName), "key name, for example, /ndn/ucla.edu/alice/DSK-123456789")
+    ("key,k", "optional, if specified, name is keyName (e.g. /ndn/ucla.edu/alice/KSK-123456789), otherwise identity name")
+    ("name,n", po::value<string>(&name), "name, for example, /ndn/ucla.edu/alice")
     ;
+
+  po::positional_options_description p;
+  p.add("name", 1);
   
   po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
   po::notify(vm);
 
   if (vm.count("help")) 
@@ -45,26 +50,45 @@ int main(int argc, char** argv)
       return 1;
     }
 
-  if (0 == vm.count("key_name"))
+  if (0 == vm.count("name"))
     {
       cerr << "identity_name must be specified" << endl;
       cerr << desc << endl;
       return 1;
     }
+  
+  if (vm.count("key"))
+    isKeyName = true;
+    
 
   security::IdentityManager identityManager;
 
   try{
-    Name tmpName(keyName);
-    Name certName = tmpName.getSubName(0, tmpName.size()-1);
-    certName.append("KEY").append(tmpName.get(-1)).append("ID-CERT").append("0");
-    Ptr<security::IdentityCertificate> certificate = identityManager.getCertificate(certName);
-    Ptr<Blob> certBlob = certificate->encodeToWire();
+    if(isKeyName)
+      {
+        Name tmpName(name);
+        Name certName = tmpName.getSubName(0, tmpName.size()-1);
+        certName.append("KEY").append(tmpName.get(-1)).append("ID-CERT").append("0");
+        Ptr<security::IdentityCertificate> certificate = identityManager.getCertificate(certName);
+        Ptr<Blob> certBlob = certificate->encodeToWire();
 
-    string encoded;
-    CryptoPP::StringSource ss(reinterpret_cast<const unsigned char *>(certBlob->buf()), certBlob->size(), true,
+        string encoded;
+        CryptoPP::StringSource ss(reinterpret_cast<const unsigned char *>(certBlob->buf()), certBlob->size(), true,
                               new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded), true, 64));
-    cout << encoded;
+        cout << encoded;
+      }
+    else
+      {
+        Name identity(name);
+        Name certName = identityManager.getDefaultCertificateNameByIdentity(identity);
+        Ptr<security::IdentityCertificate> certificate = identityManager.getCertificate(certName);
+        Ptr<Blob> certBlob = certificate->encodeToWire();
+
+        string encoded;
+        CryptoPP::StringSource ss(reinterpret_cast<const unsigned char *>(certBlob->buf()), certBlob->size(), true,
+                              new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded), true, 64));
+        cout << encoded;     
+      }
   }catch(security::SecException & e){
     cerr << e.Msg() << endl;
     return 1;
