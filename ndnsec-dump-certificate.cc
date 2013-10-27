0e11e6c5caae7d46ea0d5e4a9768a4d138d09f83
Yingdi Yu <yuyingdi@gmail.com>
2013-10-27 16:16:12 -0700

    Adding the ability of displaying certificate content in ndnsec-dump-certificate

---
+++ ndnsec-dump-certificate.cc
@@ -0,0 +1,123 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ *                     Yingdi Yu
+ *
+ * BSD license, See the LICENSE file for more information
+ *
+ * Author: Yingdi Yu <yingdi@cs.ucla.edu>
+ */
+
+#include <iostream>
+#include <fstream>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <cryptopp/base64.h>
+
+#include "ndn.cxx/security/identity/identity-manager.h"
+#include "ndn.cxx/security/certificate/identity-certificate.h"
+#include "ndn.cxx/security/certificate/certificate-subdescrpt.h"
+#include "ndn.cxx/security/exception.h"
+
+using namespace std;
+using namespace ndn;
+namespace po = boost::program_options;
+
+
+int main(int argc, char** argv)	
+{
+  string name;
+  bool isKeyName = false;
+  bool isPretty = false;
+
+  po::options_description desc("General Usage\n  ndn-dump-certificate [-h] [-p] [-k] name\nGeneral options");
+  desc.add_options()
+    ("help,h", "produce help message")
+    ("pretty,p", "optional, if specified, display certificate in human readable format")
+    ("key,k", "optional, if specified, name is keyName (e.g. /ndn/ucla.edu/alice/KSK-123456789), otherwise identity name")
+    ("name,n", po::value<string>(&name), "name, for example, /ndn/ucla.edu/alice")
+    ;
+
+  po::positional_options_description p;
+  p.add("name", 1);
+  
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) 
+    {
+      cerr << desc << endl;
+      return 1;
+    }
+
+  if (0 == vm.count("name"))
+    {
+      cerr << "identity_name must be specified" << endl;
+      cerr << desc << endl;
+      return 1;
+    }
+  
+  if (vm.count("key"))
+    isKeyName = true;
+  if (vm.count("pretty"))
+    isPretty = true;
+    
+
+  security::IdentityManager identityManager;
+  Ptr<security::IdentityCertificate> certificate;
+
+  try{
+    if(isKeyName)
+      {
+	Name certName = identityManager.getPublicStorage()->getDefaultCertificateNameForKey(name);
+        certificate = identityManager.getCertificate(certName);
+      }
+    else
+      {
+	Name certName = identityManager.getDefaultCertificateNameByIdentity(name);
+        certificate = identityManager.getCertificate(certName);
+      }
+    
+    if(NULL == certificate)
+      {
+        cerr << "No certificate found!" << endl;
+        return 1;
+      }
+
+    if(isPretty)
+      {
+        cout << "Certificate name: " << endl;
+        cout << "  " << certificate->getName() << endl;
+        cout << "Validity: " << endl;
+        cout << "  NotBefore: " << boost::posix_time::to_simple_string(certificate->getNotBefore()) << endl;
+        cout << "  NotAfter: " << boost::posix_time::to_simple_string(certificate->getNotAfter()) << endl;
+        cout << "Subject Description: " << endl;
+        const vector<security::CertificateSubDescrypt>& SubDescriptionList = certificate->getSubjectDescriptionList();
+        vector<security::CertificateSubDescrypt>::const_iterator it = SubDescriptionList.begin();
+        for(; it != SubDescriptionList.end(); it++)
+          cout << "  " << it->getOidStr() << ": " << it->getValue() << endl;
+        cout << "Public key bits: " << endl;
+        const Blob& keyBlob = certificate->getPublicKeyInfo().getKeyBlob();
+        string encoded;
+        CryptoPP::StringSource ss(reinterpret_cast<const unsigned char *>(keyBlob.buf()), keyBlob.size(), true,
+                                  new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded), true, 64));
+        cout << encoded;        
+      }
+    else
+      {
+        Ptr<Blob> certBlob = certificate->encodeToWire();
+        string encoded;
+        CryptoPP::StringSource ss(reinterpret_cast<const unsigned char *>(certBlob->buf()), certBlob->size(), true,
+                              new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded), true, 64));
+        cout << encoded;
+      }
+  }catch(security::SecException & e){
+    cerr << e.Msg() << endl;
+    return 1;
+  }
+  return 0;
+}
